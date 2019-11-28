bb1a9965d15cfc47fee15a1506eecd2a20fb8737
liamgam <liam.liamgamble@gmail.com>
2019-11-28 14:55:14 +0200

    Initial Commit

---
+++ KMAUI/KMAUI.h
@@ -0,0 +1,19 @@
+//
+//  KMAUI.h
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 28.11.2019.
+//  Copyright © 2019 Stanislav Rastvorov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KMAUI.
+FOUNDATION_EXPORT double KMAUIVersionNumber;
+
+//! Project version string for KMAUI.
+FOUNDATION_EXPORT const unsigned char KMAUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMAUI/PublicHeader.h>
+
+
