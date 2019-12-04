131095ee4074ff6366f5ec6cdd122e4ffe712d14
liamgam <liam.liamgamble@gmail.com>
2019-12-04 19:45:23 +0200

    Save button cell created

---
+++ KMAUI/UI Items/Cells/KMAUISaveButtonTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  KMAUISaveButtonTableViewCell.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 04.12.2019.
+//  Copyright © 2019 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+public class KMAUISaveButtonTableViewCell: UITableViewCell {
+    // MARK: - IBOutlets
+    @IBOutlet public weak var saveButton: UIButton!
+    
+    // MARK: - Variables
+    public var buttonPressedCallback: ((String) -> Void)?
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Rounded corners for button
+        saveButton.layer.cornerRadius = 29
+        saveButton.clipsToBounds = true
+        
+        // No selection required
+        selectionStyle = .none
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction public func saveButtonPressed(_ sender: Any) {
+        buttonPressedCallback?("pressed")
+    }
+}
