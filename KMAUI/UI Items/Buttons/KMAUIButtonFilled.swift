3a52266dcd88debc859817b232679e98390850c1
liamgam <liam.liamgamble@gmail.com>
2019-12-11 17:01:03 +0200

    KMAUIPropertyDataTableViewCell added

---
+++ KMAUI/UI Items/Buttons/KMAUIButtonFilled.swift
@@ -0,0 +1,24 @@
+//
+//  KMAButtonFilled.swift
+//  KMA EYES CITIZENS
+//
+//  Created by Stanislav Rastvorov on 8/29/19.
+//  Copyright © 2019 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+/// This class represents the button with the green background and white title
+
+public class KMAButtonFilled: UIButton {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        titleLabel?.font = UIFont.systemFont(ofSize: 15.0, weight: .semibold)
+        setTitleColor(UIColor.white, for: .normal) // It should be white for both Light and Dark mode
+        backgroundColor = KMAUIConstants.shared.KMABrightBlueColor
+        layer.cornerRadius = KMAConstants.shared.KMACornerRadius
+        clipsToBounds = true
+    }
+}
