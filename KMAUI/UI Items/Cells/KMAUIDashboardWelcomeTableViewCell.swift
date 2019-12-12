c65c92445638a0e5bc40f398fa35622d79bd18ac
liamgam <liam.liamgamble@gmail.com>
2019-12-12 15:32:38 +0200

    Dashboard Welcome cell added

---
+++ KMAUI/UI Items/Cells/KMAUIDashboardWelcomeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  KMAUIDashboardWelcomeTableViewCell.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 12.12.2019.
+//  Copyright © 2019 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+public class KMAUIDashboardWelcomeTableViewCell: UITableViewCell {
+    // MARK: - IBOutlets
+    @IBOutlet public weak var welcomeTitleLabel: KMAUITitleLabel!
+    @IBOutlet public weak var welcomeTextLabel: KMAUITextLabel!
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
