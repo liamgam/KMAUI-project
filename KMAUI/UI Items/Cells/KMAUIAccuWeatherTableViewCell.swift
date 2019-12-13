b6ec5d7d0380e97b5193a4dfdb03fdfd244ba6ca
liamgam <liam.liamgamble@gmail.com>
2019-12-13 16:08:08 +0200

    Dashboard Welcome cell added

---
+++ KMAUI/UI Items/Cells/KMAUIAccuWeatherTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  KMAUIAccuWeatherTableViewCell.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 13.12.2019.
+//  Copyright © 2019 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+public class KMAUIAccuWeatherTableViewCell: UITableViewCell {
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // No selection required
+        selectionStyle = .none
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
