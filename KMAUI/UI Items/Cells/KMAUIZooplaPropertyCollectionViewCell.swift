15eabf749a360a6281806be908b5d77b3805df9f
liamgam <liam.liamgamble@gmail.com>
2019-12-17 19:20:42 +0200

    CollectionViewCell height adjusted

--- KMAUI/UI Items/Cells/KMAUIZooplaPropertyCollectionViewCell.swift
+++ KMAUI/UI Items/Cells/KMAUIZooplaPropertyCollectionViewCell.swift
@@ -42,7 +42,7 @@ public class KMAUIZooplaPropertyCollectionViewCell: UICollectionViewCell {
         } else {
             priceLabel.text = "  " + detailStrings.1 + " "
             priceLabel.layer.cornerRadius = KMAUIConstants.shared.KMACornerRadius
-            priceLabel.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+            priceLabel.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
             priceLabel.alpha = 1
         }
     }
