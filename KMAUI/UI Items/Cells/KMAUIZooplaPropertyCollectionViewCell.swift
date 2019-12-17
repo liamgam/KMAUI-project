a521b6641073ca86bf4d0a7bcf5eb2b299201089
liamgam <liam.liamgamble@gmail.com>
2019-12-17 15:18:14 +0200

    Collection view for property items

--- KMAUI/UI Items/Cells/KMAUIZooplaPropertyCollectionViewCell.swift
+++ KMAUI/UI Items/Cells/KMAUIZooplaPropertyCollectionViewCell.swift
@@ -11,6 +11,10 @@ import UIKit
 public class KMAUIZooplaPropertyCollectionViewCell: UICollectionViewCell {
     // MARK: - IBOutlets
     @IBOutlet public weak var propertyImageView: UIImageView!
+    @IBOutlet public weak var propertyInfoLabel: KMAUIInfoLabel!
+    
+    // MARK: - Variables
+    var propertyObject = KMAZooplaProperty()
     
     override public func awakeFromNib() {
         super.awakeFromNib()
@@ -22,5 +26,50 @@ public class KMAUIZooplaPropertyCollectionViewCell: UICollectionViewCell {
         propertyImageView.layer.borderColor = KMAUIConstants.shared.KMALineGray.cgColor
         propertyImageView.layer.borderWidth = KMAUIConstants.shared.KMABorderWidthLight
         propertyImageView.image = KMAUIConstants.shared.propertyIcon.withRenderingMode(.alwaysTemplate)
+    }
+    
+    /**
+     Setup the data for cell
+     */
+    
+    public func setupCell() {
+        var propertyDescription = ""
+                    
+                    // bedrooms
+                    if propertyObject.numBedrooms > 0 {
+                        propertyDescription = "\(propertyObject.numBedrooms) bedroom"
+                    }
+                    
+                    // type
+                    if !propertyObject.type.isEmpty {
+                        if propertyDescription.isEmpty {
+                            propertyDescription = propertyObject.type
+                        } else {
+                            propertyDescription += " " + propertyObject.type.lowercased()
+                        }
+                    }
+                    
+                    // status
+                    if !propertyObject.status.isEmpty {
+                        if propertyDescription.isEmpty {
+                            if propertyObject.status == "rent" {
+                                propertyDescription = "To rent, £\(propertyObject.rentMonth.withCommas()) pcm"
+                            } else if propertyObject.status == "sale" {
+                                propertyDescription = "For sale, £\(propertyObject.salePrice.withCommas())"
+                            }
+                        } else {
+                            if propertyObject.status == "rent" {
+                                propertyDescription += " to rent, £\(propertyObject.rentMonth.withCommas()) pcm"
+                            } else if propertyObject.status == "sale" {
+                                propertyDescription += " for sale, £\(propertyObject.salePrice.withCommas())"
+                            }
+                        }
+                    }
+                    
+        if propertyDescription.isEmpty {
+            propertyInfoLabel.text = "Property"
+        } else {
+            propertyInfoLabel.text = propertyDescription
+        }
     }
 }
