7124097376a6dcc41c26b506a9812479fdd102eb
liamgam <liam.liamgamble@gmail.com>
2019-12-19 18:13:16 +0200

    Property info cell

--- KMAUI/UI Items/Cells/Zoopla/KMAUIZooplaPropertyInfoTableViewCell.swift
+++ KMAUI/UI Items/Cells/Zoopla/KMAUIZooplaPropertyInfoTableViewCell.swift
@@ -21,13 +21,12 @@ public class KMAUIZooplaPropertyInfoTableViewCell: UITableViewCell {
     @IBOutlet public weak var receptCountLabel: KMAUIInfoLabel!
     
     // MARK: - Variables
-    public var property = KMAZooplaProperty()
+    public var zooplaProperty = KMAZooplaProperty()
+    
+    // MARK: - Cell methods
     
     override public func awakeFromNib() {
         super.awakeFromNib()
-
-        // No selection required
-        selectionStyle = .none
         
         // Setting the color for images
         bedIcon.image = KMAUIConstants.shared.bedIcon.withRenderingMode(.alwaysTemplate)
@@ -36,6 +35,9 @@ public class KMAUIZooplaPropertyInfoTableViewCell: UITableViewCell {
         bathIcon.tintColor = KMAUIConstants.shared.KMALineGray
         receptIcon.image = KMAUIConstants.shared.receptIcon.withRenderingMode(.alwaysTemplate)
         bathIcon.tintColor = KMAUIConstants.shared.KMALineGray
+        
+        // No selection required
+        selectionStyle = .none
     }
 
     override public func setSelected(_ selected: Bool, animated: Bool) {
@@ -46,18 +48,18 @@ public class KMAUIZooplaPropertyInfoTableViewCell: UITableViewCell {
     
     public func setupCell() {
         // Get details data
-        let info = property.getPropertyDescription()
+        let info = zooplaProperty.getPropertyDescription()
         // price: sale / rent
         priceLabel.text = info.1
         // name
         nameLabel.text = info.0
         // address
-        addressLabel.text = property.address
+        addressLabel.text = zooplaProperty.address
         // bedrooms
-        bedCountLabel.text = "\(property.numBedrooms)"
+        bedCountLabel.text = "\(zooplaProperty.numBedrooms)"
         // bathrooms
-        bathCountLabel.text = "\(property.numBathrooms)"
+        bathCountLabel.text = "\(zooplaProperty.numBathrooms)"
         // receptions
-        receptCountLabel.text = "\(property.numReceptions)"
+        receptCountLabel.text = "\(zooplaProperty.numReceptions)"
     }
 }
