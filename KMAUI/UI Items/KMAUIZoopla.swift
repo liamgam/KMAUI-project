5c75946edbf04a71698efe4e415aa55735de70c4
liamgam <liam.liamgamble@gmail.com>
2019-12-19 11:54:09 +0200

    Testing the inner pod integration

--- KMAUI/UI Items/KMAUIZoopla.swift
+++ KMAUI/UI Items/KMAUIZoopla.swift
@@ -102,6 +102,29 @@ public class KMAUIZoopla {
             }
         }
     }
+    
+    /**
+     Get the property pins inside the provided bounds
+     */
+    
+    public func zooplaProperty(bounds: String, completion: @escaping (_ property: [JSON], _ error: String)->()) {
+        // Preparing the request string
+        let requestString = "https://api.zoopla.co.uk/api/v1/property_listings.js?\(bounds)&page_size=100&api_key=z2jerfddwxkye3w653muzfjy"
+        // The venues request
+        AF.request(requestString).responseJSON { response in
+            if let responseData = response.data {
+                do {
+                    let json = try JSON(data: responseData)
+                    
+                    if let listing = json["listing"].array {
+                        completion(listing, "")
+                    }
+                } catch {
+                    print([JSON](), error.localizedDescription)
+                }
+            }
+        }
+    }
 }
 
 // MARK: - Zoopla Propety struct
