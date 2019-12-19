6441fcf7b35dcb84593630d65be0fb5bd759362d
liamgam <liam.liamgamble@gmail.com>
2019-12-19 13:03:40 +0200

    Testing the inner pod integration

--- KMAUI/UI Items/KMAUIAccuWeather.swift
+++ KMAUI/UI Items/KMAUIAccuWeather.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class KMAUIAccuWeather {
+public class KMAUIAccuWeather {
     
     // Access variable
     public static let shared = KMAUIAccuWeather()
