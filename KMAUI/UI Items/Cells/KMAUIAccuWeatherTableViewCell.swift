b997b2ff771cfc97e30a2ac9d53fbc2a3eff4a3a
liamgam <liam.liamgamble@gmail.com>
2019-12-19 14:54:48 +0200

    Testing the inner pod integration

--- KMAUI/UI Items/Cells/KMAUIAccuWeatherTableViewCell.swift
+++ KMAUI/UI Items/Cells/KMAUIAccuWeatherTableViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 public class KMAUIAccuWeatherTableViewCell: UITableViewCell {
     @IBOutlet public weak var weatherIcon: UIImageView!
@@ -27,4 +28,26 @@ public class KMAUIAccuWeatherTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    public func setLoading() {
+        weatherTitleLabel.text = "Neighbourhood weather"
+        weatherTextLabel.text = "Preparing details..."
+        activityView.alpha = 1
+        activityView.startAnimating()
+        weatherIcon.alpha = 0
+    }
+    
+    public func setWeather(weatherObject: KMAWeather) {
+        weatherTitleLabel.text = weatherObject.title
+        weatherTextLabel.text = weatherObject.text
+        weatherIcon.alpha = 1
+        activityView.alpha = 0
+        
+        if weatherObject.image == "Error" {
+            weatherIcon.tintColor = KMAUIConstants.shared.KMALineGray
+            weatherIcon.image = KMAUIConstants.shared.weatherError.withRenderingMode(.alwaysTemplate)
+        } else if let url = URL(string: weatherObject.image) {
+            weatherIcon.kf.indicatorType = .activity
+            weatherIcon.kf.setImage(with: url)
+        }
+    }
 }
