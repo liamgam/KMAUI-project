1a7c8f5b08500d590d44e84b7a90eb633bd7881c
liamgam <liam.liamgamble@gmail.com>
2019-12-20 12:41:43 +0200

    Property image cell

---
+++ KMAUI/UI Items/Cells/Zoopla/KMAUIZooplaPricesCompareTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  KMAUIZooplaPricesCompareTableViewCell.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 20.12.2019.
+//  Copyright © 2019 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+class KMAUIZooplaPricesCompareTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // No selection required
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
