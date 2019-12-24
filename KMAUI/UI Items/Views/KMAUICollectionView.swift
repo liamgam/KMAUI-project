102b8c2cba0659f799a60df4a6fcadab0f042584
liamgam <liam.liamgamble@gmail.com>
2019-12-24 11:32:25 +0200

    Foursquare venue details

---
+++ KMAUI/UI Items/Views/KMAUICollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  KMAUICollectionView.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 24.12.2019.
+//  Copyright © 2019 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+public class KMAUICollectionView: UICollectionView {
+    // MARK: - Variables
+    var type = ""
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
