4ca98832bd282aa1d6110b641da0028e80acb427
liamgam <liam.liamgamble@gmail.com>
2019-12-24 11:07:47 +0200

    Foursquare venue details

--- KMAUI/UI Items/Cells/Dashboard/KMAUIDashboardAnalysisTableViewCell.swift
+++ KMAUI/UI Items/Cells/Dashboard/KMAUIDashboardAnalysisTableViewCell.swift
@@ -10,10 +10,15 @@ import UIKit
 
 public class KMAUIDashboardAnalysisTableViewCell: UITableViewCell {
     // MARK: - IBOutlets
+    @IBOutlet weak var rentTitleLabel: KMAUITextLabel!
     @IBOutlet public weak var rentLabel: KMAUITitleLabel!
+    @IBOutlet weak var saleTitleLabel: KMAUITextLabel!
     @IBOutlet public weak var saleLabel: KMAUITitleLabel!
     @IBOutlet public weak var reviewButton: KMAUIButtonFilled!
     @IBOutlet public weak var collectionView: UICollectionView!
+    @IBOutlet public weak var collectionViewTop: NSLayoutConstraint!
+    
+    // 22 8 22 8
     
     // MARK: - Variables
     public var buttomPressedCallback: ((Bool) -> Void)?
@@ -39,10 +44,27 @@ public class KMAUIDashboardAnalysisTableViewCell: UITableViewCell {
     }
     
     public func setupProperty() {
+        // Top offset for collectionView
+        collectionViewTop.constant = 8
         // Property rent and sale analysis
         let propertyAnalysis = KMAUIZoopla.shared.getAverage(propertyArray: property)
         rentLabel.text = propertyAnalysis.0
         saleLabel.text = propertyAnalysis.1
+        // Show labels
+        rentTitleLabel.alpha = 1
+        rentLabel.alpha = 1
+        saleTitleLabel.alpha = 1
+        saleLabel.alpha = 1
+    }
+    
+    public func setupVenues() {
+        // Top offset for collectionView
+        collectionViewTop.constant = -52
+        // Hide labels
+        rentTitleLabel.alpha = 0
+        rentLabel.alpha = 0
+        saleTitleLabel.alpha = 0
+        saleLabel.alpha = 0
     }
     
     @IBAction public func reviewButtonPressed(_ sender: Any) {
