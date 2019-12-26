14630fc843c74c3f8a6df6815c16ce12910d0d12
liamgam <liam.liamgamble@gmail.com>
2019-12-26 19:14:45 +0200

    Foursquare venue details

---
+++ KMAUI/UI Items/Cells/Dashboard/Foursquare/KMAUIFoursquareContactsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  KMAUIFoursquareContactsTableViewCell.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 26.12.2019.
+//  Copyright © 2019 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+class KMAUIFoursquareContactsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
