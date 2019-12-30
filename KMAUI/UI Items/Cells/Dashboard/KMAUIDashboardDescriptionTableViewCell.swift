3003da2a35a9a4b6e901c9642607dc0fffabfbe9
liamgam <liam.liamgamble@gmail.com>
2019-12-30 17:45:36 +0200

    Foursquare venue details

--- KMAUI/UI Items/Cells/Dashboard/KMAUIDashboardDescriptionTableViewCell.swift
+++ KMAUI/UI Items/Cells/Dashboard/KMAUIDashboardDescriptionTableViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public class KMAUIZooplaPropertyDescriptionTableViewCell: UITableViewCell {
+public class KMAUIDashboardDescriptionTableViewCell: UITableViewCell {
     // MARK: - IBOutlets
     @IBOutlet weak var propertyTitleLabel: KMAUITitleLabel!
     @IBOutlet public weak var propertyDescriptionLabel: KMAUIInfoLabel!
