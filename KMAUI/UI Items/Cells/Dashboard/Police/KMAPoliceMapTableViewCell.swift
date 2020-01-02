d19eed3e889b524c2d911cd981aa0af988fe0c79
liamgam <liam.liamgamble@gmail.com>
2020-01-02 12:36:26 +0200

    Police API details

--- KMAUI/UI Items/Cells/Dashboard/Police/KMAPoliceMapTableViewCell.swift
+++ KMAUI/UI Items/Cells/Dashboard/Police/KMAPoliceMapTableViewCell.swift
@@ -40,14 +40,14 @@ public class KMAPoliceMapTableViewCell: UITableViewCell {
     public func setupCell() {
         print("Nearby crime: \(neighbourhood.crimeNearby.count), crime: \(neighbourhood.crimeArray.count)")
 
-//        let point1 = CLLocationCoordinate2D(latitude: neighbourhood.minLat, longitude: neighbourhood.minLong)
-//        let point2 = CLLocationCoordinate2D(latitude: neighbourhood.minLat, longitude: neighbourhood.maxLong)
-//        let point3 = CLLocationCoordinate2D(latitude: neighbourhood.maxLat, longitude: neighbourhood.maxLong)
-//        let point4 = CLLocationCoordinate2D(latitude: neighbourhood.maxLat, longitude: neighbourhood.minLong)
-//        let rectBounds = [point1, point2, point3, point4, point1]
-//
-//        let rectPolygon = KMAUIUtilities.shared.getPolygon(bounds: rectBounds)
-//        mapView.addOverlay(rectPolygon)
+        let point1 = CLLocationCoordinate2D(latitude: neighbourhood.minLat, longitude: neighbourhood.minLong)
+        let point2 = CLLocationCoordinate2D(latitude: neighbourhood.minLat, longitude: neighbourhood.maxLong)
+        let point3 = CLLocationCoordinate2D(latitude: neighbourhood.maxLat, longitude: neighbourhood.maxLong)
+        let point4 = CLLocationCoordinate2D(latitude: neighbourhood.maxLat, longitude: neighbourhood.minLong)
+        let rectBounds = [point1, point2, point3, point4, point1]
+
+        let rectPolygon = KMAUIUtilities.shared.getPolygon(bounds: rectBounds)
+        mapView.addOverlay(rectPolygon)
         
         let polygon = KMAUIUtilities.shared.getPolygon(bounds: neighbourhood.bounds)
         mapView.addOverlay(polygon)
@@ -59,12 +59,12 @@ public class KMAPoliceMapTableViewCell: UITableViewCell {
             mapView.addAnnotation(annotation)
         }
         
-//        for crimeObject in neighbourhood.crimeNearby {
-//            let annotation = MKPointAnnotation()
-//            annotation.coordinate = crimeObject.location
-//            annotation.title = crimeObject.category.capitalized.replacingOccurrences(of: "-", with: " ")
-//            mapView.addAnnotation(annotation)
-//        }
+        for crimeObject in neighbourhood.crimeNearby {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = crimeObject.location
+            annotation.title = crimeObject.category.capitalized.replacingOccurrences(of: "-", with: " ")
+            mapView.addAnnotation(annotation)
+        }
 
         // Set the visible area
             var region = MKCoordinateRegion()
