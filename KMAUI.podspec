a568dadea83662750191286529508f8fcf71a707
liamgam <liam.liamgamble@gmail.com>
2020-01-03 17:43:00 +0200

    Police API details

--- KMAUI.podspec
+++ KMAUI.podspec
@@ -8,7 +8,7 @@ s.summary = "KMAUI allows you using the same design items across all of the KMA 
 s.requires_arc = true
 
 # 2
-s.version = "0.7.10.47"
+s.version = "0.7.10.48"
 
 # 3
 s.license = { :type => "MIT", :file => "LICENSE" }
--- KMAUI/UI Items/Cells/Dashboard/Police/KMAUIPoliceContactsTableViewCell.swift
+++ KMAUI/UI Items/Cells/Dashboard/Police/KMAUIPoliceContactsTableViewCell.swift
@@ -10,6 +10,26 @@ import UIKit
 
 public class KMAUIPoliceContactsTableViewCell: UITableViewCell {
     // MARK: - IBOutlets
+    @IBOutlet public weak var facebookLabel: KMAUITextLabel!
+    @IBOutlet public weak var facebookLabelTop: NSLayoutConstraint!
+    @IBOutlet public weak var facebookLabelHeight: NSLayoutConstraint!
+    @IBOutlet public weak var facebookButton: UIButton!
+    @IBOutlet public weak var twitterLabel: KMAUITextLabel!
+    @IBOutlet public weak var twitterLabelTop: NSLayoutConstraint!
+    @IBOutlet public weak var twitterLabelHeight: NSLayoutConstraint!
+    @IBOutlet public weak var twitterButton: UIButton!
+    @IBOutlet public weak var websiteLabel: KMAUITextLabel!
+    @IBOutlet public weak var websiteLabelTop: NSLayoutConstraint!
+    @IBOutlet public weak var websiteLabelHeight: NSLayoutConstraint!
+    @IBOutlet public weak var websiteButton: UIButton!
+    @IBOutlet public weak var emailLabel: KMAUITextLabel!
+    @IBOutlet public weak var emailLabelTop: NSLayoutConstraint!
+    @IBOutlet public weak var emailLabelHeight: NSLayoutConstraint!
+    @IBOutlet public weak var emailButton: UIButton!
+    @IBOutlet public weak var phoneLabel: KMAUITextLabel!
+    @IBOutlet public weak var phoneLabelTop: NSLayoutConstraint!
+    @IBOutlet public weak var phoneLabelHeight: NSLayoutConstraint!
+    @IBOutlet public weak var phoneButton: UIButton!
     
     // MARK: - Variables
     var neighbourhood = KMAPoliceNeighbourhood()
@@ -28,7 +48,26 @@ public class KMAUIPoliceContactsTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
+
+    public func setupCell() {
+        // Facebook
+        if !neighbourhood.facebook.isEmpty {
+            facebookButton.setTitle(neighbourhood.facebook, for: .normal)
+            facebookButton.alpha = 1
+            facebookLabel.alpha = 1
+            facebookLabelTop.constant = 8
+            facebookLabelHeight.constant = 44
+        } else {
+            facebookButton.alpha = 0
+            facebookLabel.alpha = 0
+            facebookLabelTop.constant = 0
+            facebookLabelHeight.constant = 0
+        }
+        // Twitter
+        // Website
+        // Email
+        // Phone
+    }
 }
 
 /*
