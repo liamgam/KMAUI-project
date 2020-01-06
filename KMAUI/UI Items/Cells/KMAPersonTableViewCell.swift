7f9a12f7857a66d642498957fff08303cf0e7bca
liamgam <liam.liamgamble@gmail.com>
2020-01-06 21:45:24 +0200

    Person cell

--- KMAUI/UI Items/Cells/KMAPersonTableViewCell.swift
+++ KMAUI/UI Items/Cells/KMAPersonTableViewCell.swift
@@ -13,16 +13,17 @@ import Kingfisher
 public class KMAPersonTableViewCell: UITableViewCell {
     
     // MARK: - IBOutlets
+    @IBOutlet public weak var bgView: KMAUIRoundedCornersView!
     @IBOutlet public weak var profileImageView: UIImageView!
-    @IBOutlet public weak var fullNameLabel: KMAUITextLabel!
     @IBOutlet public weak var usernameLabel: KMAUITitleLabel!
+    @IBOutlet public weak var fullNameLabel: KMAUITextLabel!
     @IBOutlet public weak var rightArrowImageView: UIImageView!
     
     // MARK: - Variables
     public var person = KMAPerson()
-
+    
     // MARK: - Cell methods
-
+    
     override public func awakeFromNib() {
         super.awakeFromNib()
         
@@ -31,6 +32,7 @@ public class KMAPersonTableViewCell: UITableViewCell {
         profileImageView.layer.borderColor = KMAUIConstants.shared.KMATextGrayColor.cgColor
         profileImageView.layer.borderWidth = KMAUIConstants.shared.KMABorderWidthLight
         profileImageView.layer.cornerRadius = KMAUIConstants.shared.KMACornerRadius
+        profileImageView.contentMode = .scaleAspectFill
         profileImageView.kf.indicatorType = .activity
         
         // Right arrow image view
@@ -39,11 +41,34 @@ public class KMAPersonTableViewCell: UITableViewCell {
         // No selection required
         selectionStyle = .none
     }
-
+    
     override public func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        setupColors(highlight: selected)
+        
+    }
+    
+    override public func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+        
+        setupColors(highlight: highlighted)
+    }
+    
+    public func setupColors(highlight: Bool) {
+        if highlight {
+            bgView.backgroundColor = KMAUIConstants.shared.KMABrightBlueColor
+            profileImageView.tintColor = UIColor.white
+            usernameLabel.textColor = UIColor.white
+            fullNameLabel.textColor = UIColor.white
+            rightArrowImageView.tintColor = UIColor.white
+        } else {
+            bgView.backgroundColor = KMAUIConstants.shared.KMABackColor
+            profileImageView.tintColor = KMAUIConstants.shared.KMATextGrayColor
+            usernameLabel.textColor = KMAUIConstants.shared.KMATitleColor
+            fullNameLabel.textColor = KMAUIConstants.shared.KMATextGrayColor
+            rightArrowImageView.tintColor = KMAUIConstants.shared.KMATextGrayColor
+        }
     }
     
     /**
