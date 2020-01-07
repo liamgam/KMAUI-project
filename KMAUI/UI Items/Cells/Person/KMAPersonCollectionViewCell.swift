2eecacfa67a539690d89bb827bb6f6e2aec4bf25
liamgam <liam.liamgamble@gmail.com>
2020-01-07 20:38:54 +0200

    Person Collection View Cell

--- KMAUI/UI Items/Cells/Person/KMAPersonCollectionViewCell.swift
+++ KMAUI/UI Items/Cells/Person/KMAPersonCollectionViewCell.swift
@@ -10,6 +10,9 @@ import UIKit
 import Charts
 
 public class KMAPersonCollectionViewCell: UICollectionViewCell {
+    // MARK: - IBOutlets
+    @IBOutlet weak var pieChartView: PieChartView!
+    
     // MARK: - Variables
     public var type = ""
     public var peopleArray = [KMAPerson]()
@@ -24,7 +27,18 @@ public class KMAPersonCollectionViewCell: UICollectionViewCell {
      */
     
     public func setupCell() {
-        
+        let entry1 = PieChartDataEntry(value: Double(243), label: "#1")
+        let entry2 = PieChartDataEntry(value: Double(232), label: "#2")
+        let entry3 = PieChartDataEntry(value: Double(123), label: "#3")
+        let dataSet = PieChartDataSet(entries: [entry1, entry2, entry3], label: "Widget Types")
+        let data = PieChartData(dataSet: dataSet)
+        pieChartView.data = data
+        pieChartView.chartDescription?.text = "Share of Widgets by Type"
+
+        //All other additions to this function will go here
+
+        //This must stay at end of function
+        pieChartView.notifyDataSetChanged()
     }
 }
 
