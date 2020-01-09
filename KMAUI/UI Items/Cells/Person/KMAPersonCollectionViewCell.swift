5d2e731055c713775c75a63569c22c7c9b906d48
liamgam <liam.liamgamble@gmail.com>
2020-01-09 22:11:33 +0200

    Person area stats

--- KMAUI/UI Items/Cells/Person/KMAPersonCollectionViewCell.swift
+++ KMAUI/UI Items/Cells/Person/KMAPersonCollectionViewCell.swift
@@ -99,7 +99,7 @@ public class KMAPersonCollectionViewCell: UICollectionViewCell {
             data.setValueFormatter(DefaultValueFormatter(formatter: pFormatter))
             
             data.setValueFont(.systemFont(ofSize: 12, weight: .regular))
-            data.setValueTextColor(.white)
+            data.setValueTextColor(KMAUIConstants.shared.KMABgGray)
             
             genderPieChartView.data = data
             genderPieChartView.notifyDataSetChanged()
@@ -245,7 +245,7 @@ public class KMAPersonCollectionViewCell: UICollectionViewCell {
             data.setValueFormatter(DefaultValueFormatter(formatter: pFormatter))
             
             data.setValueFont(.systemFont(ofSize: 12, weight: .regular))
-            data.setValueTextColor(.white)
+            data.setValueTextColor(KMAUIConstants.shared.KMABgGray)
             
             cityPieChartView.data = data
             cityPieChartView.notifyDataSetChanged()
