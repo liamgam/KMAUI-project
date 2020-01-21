85077766503fc3dfb69c48706dde95f514fe7f24
liamgam <liam.liamgamble@gmail.com>
2020-01-21 18:23:25 +0200

    Ministry UI: KMAUITotalPerformanceTableViewCell

--- KMAUI/UI Items/Ministry UI/KMAUITotalPerformanceTableViewCell.swift
+++ KMAUI/UI Items/Ministry UI/KMAUITotalPerformanceTableViewCell.swift
@@ -23,6 +23,9 @@ public class KMAUITotalPerformanceTableViewCell: UITableViewCell {
     @IBOutlet public weak var securityProgressView: RingProgressView!
     @IBOutlet public weak var securityProgressLabel: UILabel!
     
+    // MARK - Variables
+    public var regionPerformance = KMARegionPerformance()
+    
     override public func awakeFromNib() {
         super.awakeFromNib()
         
@@ -36,7 +39,43 @@ public class KMAUITotalPerformanceTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    /**
+     Setting the up the UI items
+     */
+    
     public func setupCell() {
+        // Region details
+        itemValueLabel.text = regionPerformance.regionName
+        progressPercentLabel.text = "\(regionPerformance.totalPerformance)%"
         
+        // Effectivity
+        communityProgressLabel.attributedText = KMAUIUtilities.shared.attributedText(text: "Community \(regionPerformance.performance[0])%", search: "\(regionPerformance.performance[0])%", fontSize: communityProgressLabel.font.pointSize)
+        serviceProgressLabel.attributedText = KMAUIUtilities.shared.attributedText(text: "Service \(regionPerformance.performance[1])%", search: "\(regionPerformance.performance[1])%", fontSize: serviceProgressLabel.font.pointSize)
+        securityProgressLabel.attributedText = KMAUIUtilities.shared.attributedText(text: "Security \(regionPerformance.performance[2])%", search: "\(regionPerformance.performance[2])%", fontSize: securityProgressLabel.font.pointSize)
+        
+        // Progress views
+        UIView.animate(withDuration: 0.5) {
+            self.totalProgressView.progress = Double(self.regionPerformance.totalPerformance) / 100
+            self.communityProgressView.progress = Double(self.regionPerformance.performance[0]) / 100
+            self.serviceProgressView.progress = Double(self.regionPerformance.performance[1]) / 100
+            self.securityProgressView.progress = Double(self.regionPerformance.performance[2]) / 100
+        }
+    }
+}
+
+// MARK: - Region performance struct, stores the performance percent and region name
+
+public struct KMARegionPerformance {
+    public var regionName = ""
+    public var totalPerformance = 0
+    public var performance = [Int]()
+    
+    public init() {
+    }
+    
+    public init(regionName: String, performance: [Int], totalPerformance: Int) {
+        self.regionName = regionName
+        self.performance = performance
+        self.totalPerformance = totalPerformance
     }
 }
