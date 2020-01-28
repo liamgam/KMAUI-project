05058fd5d80b521222df19661ed82f6a5b6c7b44
liamgam <liam.liamgamble@gmail.com>
2020-01-28 17:22:05 +0200

    Ministry UI: KMAUIAnalysisBlockTableViewCell

---
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUISelectableHeaderTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  KMAUISelectableHeaderTableViewCell.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 28.01.2020.
+//  Copyright © 2020 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+public class KMAUISelectableHeaderTableViewCell: UITableViewCell {
+    // MARK: - IBOutlets
+    @IBOutlet public weak var stackView: UIStackView!
+    
+    // MARK: - Variables
+    public static let id = "KMAUISelectableHeaderTableViewCell"
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
