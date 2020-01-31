a444e71405f5cb2d10e65ffeb71d9e774a069e28
liamgam <liam.liamgamble@gmail.com>
2020-01-31 15:21:46 +0200

    Ministry UI: KMAUISelectedCellsTableViewCell

--- KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIIncomeChartTableViewCell.swift
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIIncomeChartTableViewCell.swift
@@ -50,6 +50,7 @@ public class KMAUIIncomeChartTableViewCell: UITableViewCell {
         
         let xAxis = incomeChart.xAxis
         xAxis.drawAxisLineEnabled = true
+        xAxis.axisLineColor = KMAUIConstants.shared.KMAUIGreyLineColor.withAlphaComponent(0.1)
         xAxis.drawGridLinesEnabled = false
         xAxis.labelPosition = .bottom
         xAxis.valueFormatter = axisFormatDelegate
@@ -59,6 +60,7 @@ public class KMAUIIncomeChartTableViewCell: UITableViewCell {
         let leftAxis = incomeChart.leftAxis
         leftAxis.drawAxisLineEnabled = false
         leftAxis.drawGridLinesEnabled = true
+        leftAxis.gridColor = KMAUIConstants.shared.KMAUIGreyLineColor.withAlphaComponent(0.1)
         leftAxis.drawLabelsEnabled = true
         
         leftAxis.axisMinimum = 0
