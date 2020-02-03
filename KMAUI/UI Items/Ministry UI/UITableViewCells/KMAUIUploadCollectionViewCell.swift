531b3752ef83084eba7f654b0bd0830eb60df21a
liamgam <liam.liamgamble@gmail.com>
2020-02-03 16:25:14 +0200

    Ministry UI: Filter and Show on Map bar buttons

---
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIUploadCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  KMAUIUploadCollectionViewCell.swift
+//  KMA
+//
+//  Created by Stanislav Rastvorov on 03.02.2020.
+//  Copyright © 2020 Office Mac. All rights reserved.
+//
+
+import UIKit
+import KMAUI
+
+public class KMAUIUploadCollectionViewCell: UICollectionViewCell {
+    @IBOutlet public weak var bgView: UIView!
+    // MARK: - Variables
+    public static let id = "KMAUIUploadCollectionViewCell"
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+}
---
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIUploadsHorizontalTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  KMAUIUploadsHorizontalTableViewCell.swift
+//  KMA
+//
+//  Created by Stanislav Rastvorov on 03.02.2020.
+//  Copyright © 2020 Office Mac. All rights reserved.
+//
+
+import UIKit
+import KMAUI
+
+public class KMAUIUploadsHorizontalTableViewCell: UITableViewCell {
+    // MARK: - IBOutlets
+    @IBOutlet public weak var titleLabel: KMAUIBoldTextLabel!
+    @IBOutlet public weak var collectionView: UICollectionView!
+    
+    // MARK: - Variables
+    public static let id = "KMAUIUploadsHorizontalTableViewCell"
+    
+    // MARK: - Cell methods
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Set the font size for title
+        titleLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(18)
+        
+        // No selection required
+        selectionStyle = .none
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
