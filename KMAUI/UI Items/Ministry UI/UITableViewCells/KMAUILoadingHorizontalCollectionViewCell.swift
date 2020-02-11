ccdff43a534d7dc88071f14fda369b3ae7297d1f
liamgam <liam.liamgamble@gmail.com>
2020-02-11 15:36:29 +0200

    Ministry UI: KMAUIProgressView added

---
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUILoadingHorizontalCollectionViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  KMAUILoadingHorizontalCollectionViewCell.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 11.02.2020.
+//  Copyright © 2020 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+class KMAUILoadingHorizontalCollectionViewCell: UICollectionViewCell {
+    @IBOutlet public weak var bgView: UIView!
+    @IBOutlet public  weak var loadingActivityView: UIActivityIndicatorView!
+    @IBOutlet public weak var loadingLabel: UILabel!
+    
+    // MARK: - Variables
+    public static let id = "KMAUILoadingHorizontalCollectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        bgView.layer.cornerRadius = KMAUIConstants.shared.KMACornerRadius
+        bgView.clipsToBounds = true
+        
+        loadingLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(16)
+    }
+
+}
