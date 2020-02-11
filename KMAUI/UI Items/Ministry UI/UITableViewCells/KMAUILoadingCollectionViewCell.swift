919702df72a28b4bde820d8637b0687344139c04
liamgam <liam.liamgamble@gmail.com>
2020-02-11 14:48:15 +0200

    Ministry UI: KMAUIProgressView added

---
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUILoadingCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  KMAUILoadingCollectionViewCell.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 11.02.2020.
+//  Copyright © 2020 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+public class KMAUILoadingCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Variables
+    public static let id = "KMAUILoadingCollectionViewCell"
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
