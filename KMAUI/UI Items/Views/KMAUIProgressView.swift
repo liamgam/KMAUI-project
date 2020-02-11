0470cb86f9c01f0a0168a636c0d296ec5a28fc6a
liamgam <liam.liamgamble@gmail.com>
2020-02-11 13:58:49 +0200

    Ministry UI: KMAUIProgressView added

--- KMAUI/UI Items/Views/KMAUIProgressView.swift
+++ KMAUI/UI Items/Views/KMAUIProgressView.swift
@@ -8,10 +8,33 @@
 
 import UIKit
 
-class KMAUIProgressView: UIView {
-
-    required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
+public class KMAUIProgressView: UIView {
+    public var progressView = UIProgressView()
+    public var progress: Float = 0 {
+        didSet {
+            updateProgress()
+        }
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // Remove the subviews
+        for subview in self.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        // Adding the progressView
+        self.addSubview(progressView)
+        
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8).isActive = true
+        progressView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 8).isActive = true
+        progressView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
 
+    }
+    
+    public func updateProgress() {
+        progressView.progress = progress
+    }
 }
