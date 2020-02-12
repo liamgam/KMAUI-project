70ad3cf2e693d7ec098c91908e005d533fa3fe46
liamgam <liam.liamgamble@gmail.com>
2020-02-12 17:41:22 +0200

    Ministry UI: KMAUISegmentTableViewCell updated

--- KMAUI/UI Items/Ministry UI/UICollectionViewCells/KMAUILoadingCollectionViewCell.swift
+++ KMAUI/UI Items/Ministry UI/UICollectionViewCells/KMAUILoadingCollectionViewCell.swift
@@ -12,10 +12,17 @@ public class KMAUILoadingCollectionViewCell: UICollectionViewCell {
     // MARK: - IBOutlets
     @IBOutlet public weak var bgView: UIView!
     @IBOutlet public weak var loadingActivityView: UIActivityIndicatorView!
+    @IBOutlet public weak var loadingActivityViewHeight: NSLayoutConstraint!
+    @IBOutlet public weak var loadingActivityViewBottom: NSLayoutConstraint!
     @IBOutlet public weak var loadingLabel: KMAUIBoldTextLabel!
 
     // MARK: - Variables
     public static let id = "KMAUILoadingCollectionViewCell"
+    public var dataLoaded = false {
+        didSet {
+            setupCell()
+        }
+    }
 
     override public func awakeFromNib() {
         super.awakeFromNib()
@@ -28,4 +35,19 @@ public class KMAUILoadingCollectionViewCell: UICollectionViewCell {
         loadingActivityView.startAnimating()
     }
 
+    public func setupCell() {
+        if dataLoaded {
+            loadingLabel.text = "No data available..."
+            loadingActivityView.stopAnimating()
+            loadingActivityView.isHidden = true
+            loadingActivityViewHeight.constant = 0
+            loadingActivityViewBottom.constant = 0
+        } else {
+            loadingLabel.text = "Loading"
+            loadingActivityView.startAnimating()
+            loadingActivityView.isHidden = false
+            loadingActivityViewHeight.constant = 37
+            loadingActivityViewBottom.constant = 8
+        }
+    }
 }
--- KMAUI/UI Items/Ministry UI/UICollectionViewCells/KMAUILoadingHorizontalCollectionViewCell.swift
+++ KMAUI/UI Items/Ministry UI/UICollectionViewCells/KMAUILoadingHorizontalCollectionViewCell.swift
@@ -26,5 +26,4 @@ public class KMAUILoadingHorizontalCollectionViewCell: UICollectionViewCell {
         
         loadingActivityView.startAnimating()
     }
-
 }
