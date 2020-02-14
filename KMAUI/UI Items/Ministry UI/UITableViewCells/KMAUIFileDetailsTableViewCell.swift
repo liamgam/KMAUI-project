8e95b2ca13cdac4a69b2ccd841538ed53a882433
liamgam <liam.liamgamble@gmail.com>
2020-02-14 13:44:58 +0200

    Ministry UI: KMAUIUploadCollectionViewCell - selection on click

--- KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIFileDetailsTableViewCell.swift
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIFileDetailsTableViewCell.swift
@@ -12,7 +12,7 @@ public class KMAUIFileDetailsTableViewCell: UITableViewCell {
     // MARK: - IBOutlets
     @IBOutlet public weak var profileImageView: UIImageView!
     @IBOutlet public weak var nameLabel: KMAUIBoldTextLabel!
-    @IBOutlet public weak var uploadDateLabel: KMAUIInfoLabel!
+    @IBOutlet public weak var dateLabel: KMAUIInfoLabel!
     @IBOutlet public weak var fileImageView: UIImageView!
     
     // MARK: - Variables
@@ -35,6 +35,45 @@ public class KMAUIFileDetailsTableViewCell: UITableViewCell {
     }
     
     public func setupCell() {
+        // Citizen image
+        profileImageView.image = KMAUIConstants.shared.profileIcon.withRenderingMode(.alwaysTemplate)
+        profileImageView.tintColor = KMAUIConstants.shared.KMALineGray
+        profileImageView.alpha = 0.25
+        profileImageView.layer.cornerRadius = 0
         
+        if !uploadItem.citizenImage.isEmpty, let url = URL(string: uploadItem.citizenImage) {
+            profileImageView.kf.setImage(with: url) { result in
+                switch result {
+                case .success(let value):
+                    self.profileImageView.image = value.image
+                    self.profileImageView.alpha = 1
+                    self.profileImageView.layer.cornerRadius = 20
+                case .failure(let error):
+                    print(error.localizedDescription) // The error happens
+                }
+            }
+        }
+        
+        // Citizen name
+        nameLabel.text = uploadItem.citizenName
+        
+        // Upload date
+        dateLabel.text = KMAUIUtilities.shared.formatStringShort(date: uploadItem.uploadDate, numOnly: true)
+        
+        // Upload image
+        fileImageView.image = KMAUIConstants.shared.placeholderUploadImageNoir
+        fileImageView.alpha = 0.25
+        
+        if !uploadItem.uploadImage.isEmpty, let url = URL(string: uploadItem.uploadImage) {
+            fileImageView.kf.setImage(with: url) { result in
+                switch result {
+                case .success(let value):
+                    self.fileImageView.image = value.image
+                    self.fileImageView.alpha = 1
+                case .failure(let error):
+                    print(error.localizedDescription) // The error happens
+                }
+            }
+        }
     }
 }
