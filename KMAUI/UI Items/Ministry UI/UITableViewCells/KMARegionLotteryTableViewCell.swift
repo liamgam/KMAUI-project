b65be869642ee063262fbec0e86debfc18b8b173
liamgam <liam.liamgamble@gmail.com>
2020-03-02 17:56:06 +0200

    Ministry UI: KMARegionLotteryTableViewCell moved to KMAUI

---
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMARegionLotteryTableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  KMARegionLotteryTableViewCell.swift
+//  KMA EYES CITIZENS
+//
+//  Created by Stanislav Rastvorov on 02.03.2020.
+//  Copyright © 2020 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+public class KMARegionLotteryTableViewCell: UITableViewCell {
+    // MARK: - IBOutlets
+    @IBOutlet public weak var regionNameLabel: KMAUIBoldTextLabel!
+    @IBOutlet public weak var subLandsCountLabel: KMAUIRegularTextLabel!
+    @IBOutlet public weak var subLandsCountLabelHeight: NSLayoutConstraint!
+    @IBOutlet public weak var queueButton: KMAUIButtonFilled!
+    
+    // MARK: - Variables
+    public static let id = "KMARegionLotteryTableViewCell"
+    public var region = KMAMapAreaStruct()
+    public var joined = false {
+           didSet {
+               setupCell()
+           }
+       }
+    public var queueCallback: ((Bool) -> Void)?
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Cell background color
+        backgroundColor = KMAUIConstants.shared.KMAUIViewBgColor
+        
+        // Border for queueButton
+        queueButton.layer.borderColor = KMAUIConstants.shared.KMABrightBlueColor.cgColor
+        queueButton.layer.borderWidth = KMAUIConstants.shared.KMABorderWidthRegular
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // Setup cell
+    
+    public func setupCell() {
+        regionNameLabel.text = region.nameE
+        
+        if region.landPlans.isEmpty {
+            subLandsCountLabel.text = "Queue: \(region.lotteryMembersCount)"
+        } else {
+            subLandsCountLabel.text = "Land Plans: \(region.landPlans.count), queue: \(region.lotteryMembersCount)"
+        }
+        
+        if joined {
+            queueButton.backgroundColor = KMAUIConstants.shared.KMAUIViewBgColor
+            queueButton.setTitleColor(KMAUIConstants.shared.KMABrightBlueColor, for: .normal)
+            queueButton.setTitle("Lottery joined", for: .normal)
+        } else {
+            queueButton.backgroundColor = KMAUIConstants.shared.KMABrightBlueColor
+            queueButton.setTitleColor(UIColor.white, for: .normal)
+            queueButton.setTitle("Join lottery", for: .normal)
+        }
+    }
+
+    // MARK: - IBActions
+    
+    @IBAction public func joinQueueButtonPressed(_ sender: Any) {
+        queueCallback?(true)
+    }
+}
