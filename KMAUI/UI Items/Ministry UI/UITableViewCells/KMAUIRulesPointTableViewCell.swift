1c7dac74641dc35bf2b51c3b0f0f614bf0b3d03a
liamgam <liam.liamgamble@gmail.com>
2020-03-12 12:52:39 +0200

    Ministry UI: KMAUILotteryRule struct

--- KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIRulesPointTableViewCell.swift
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIRulesPointTableViewCell.swift
@@ -23,7 +23,9 @@ public class KMAUIRulesPointTableViewCell: UITableViewCell {
     
     override public func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        // No selection required
+        selectionStyle = .none
     }
 
     override public func setSelected(_ selected: Bool, animated: Bool) {
