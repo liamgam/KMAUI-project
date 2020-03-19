82a23841762e8316fe4b9bf5b484e5cafeee4cee
liamgam <liam.liamgamble@gmail.com>
2020-03-19 17:58:53 +0200

    Ministry UI: Tab Bar images updated

--- KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIPropertyTableViewCell.swift
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIPropertyTableViewCell.swift
@@ -14,6 +14,8 @@ public class KMAUIPropertyTableViewCell: UITableViewCell {
     @IBOutlet public weak var ownershipLabel: KMAUIRegularTextLabel!
     @IBOutlet public weak var circleViewOne: UIView!
     @IBOutlet public weak var residentsLabel: KMAUIRegularTextLabel!
+    @IBOutlet public weak var circleViewTwo: UIView!
+    @IBOutlet public weak var addressLabel: KMAUIRegularTextLabel!
     
     // MARK: - Variables
     public static let id = "KMAUIPropertyTableViewCell"
@@ -22,7 +24,7 @@ public class KMAUIPropertyTableViewCell: UITableViewCell {
             setupCell()
         }
     }
-
+    
     override public func awakeFromNib() {
         super.awakeFromNib()
         
@@ -32,17 +34,19 @@ public class KMAUIPropertyTableViewCell: UITableViewCell {
         // Ownership label
         ownershipLabel.font = KMAUIConstants.shared.KMAUIRegularFont.withSize(16)
         
-        // Circle view 1
+        // Circle views
         circleViewOne.layer.cornerRadius = 2.5
         circleViewOne.clipsToBounds = true
+        circleViewTwo.layer.cornerRadius = 2.5
+        circleViewTwo.clipsToBounds = true
         
         // No selection required
         selectionStyle = .none
     }
-
+    
     override public func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
@@ -62,6 +66,13 @@ public class KMAUIPropertyTableViewCell: UITableViewCell {
             residentsLabel.text = "1 resident"
         } else {
             residentsLabel.text = "\(property.residentsCount) residents"
+        }
+        
+        // Address
+        if property.formattedAddress.isEmpty {
+            addressLabel.text = "No address"
+        } else {
+            addressLabel.text = property.formattedAddress
         }
     }
 }
