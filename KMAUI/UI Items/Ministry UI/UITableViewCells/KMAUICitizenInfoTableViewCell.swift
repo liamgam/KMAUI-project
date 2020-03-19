6662d8931ef4a88f81c15451be94bf54e182ce9b
liamgam <liam.liamgamble@gmail.com>
2020-03-19 13:36:54 +0200

    Ministry UI: Tab Bar images updated

--- KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUICitizenInfoTableViewCell.swift
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUICitizenInfoTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  KMACitizenInfoTableViewCell.swift
+//  KMAUICitizenInfoTableViewCell.swift
 //  KMAUI
 //
 //  Created by Stanislav Rastvorov on 19.03.2020.
@@ -9,19 +9,11 @@
 import UIKit
 
 public class KMAUICitizenInfoTableViewCell: UITableViewCell {
-    // MARK: - IBOutlets
-    @IBOutlet public weak var bgView: KMAUIRoundedCornersView!
-    @IBOutlet public weak var citizenView: UIView!
-
     // MARK: - Variables
     public static let id = "KMAUICitizenInfoTableViewCell"
 
     override public func awakeFromNib() {
         super.awakeFromNib()
-        
-        // Larger shadow for bgView
-        bgView.layer.shadowOffset = CGSize(width: 0, height: 0)
-        bgView.layer.shadowRadius = 12
         
         // No selection required
         selectionStyle = .none
