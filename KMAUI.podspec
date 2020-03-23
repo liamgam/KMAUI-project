e2e2ef17a745b4941a705a39255b05fb45ec0f26
liamgam <liam.liamgamble@gmail.com>
2020-03-23 13:30:42 +0200

    Ministry UI: the new KMAUILotteryResultTableViewCell cell created

--- KMAUI.podspec
+++ KMAUI.podspec
@@ -8,7 +8,7 @@ s.summary = "KMAUI allows you using the same design items across all of the KMA 
 s.requires_arc = true
 
 # 2
-s.version = "0.7.13.691"
+s.version = "0.7.13.692"
 
 # 3
 s.license = { :type => "MIT", :file => "LICENSE" }
---
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUILotteryResultTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  KMAUILotteryResultTableViewCell.swift
+//  KMAUI
+//
+//  Created by Stanislav Rastvorov on 23.03.2020.
+//  Copyright © 2020 Stanislav Rastvorov. All rights reserved.
+//
+
+import UIKit
+
+public class KMAUILotteryResultTableViewCell: UITableViewCell {
+    // MARK: - Variables
+    public static let id = "KMAUILotteryResultTableViewCell"
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
