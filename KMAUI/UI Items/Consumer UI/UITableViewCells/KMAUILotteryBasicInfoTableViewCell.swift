5b15accce4d3cda8332c31b9b949906018b2b7bc
liamgam <liam.liamgamble@gmail.com>
2020-04-14 17:14:01 +0300

    Lottery status for KMAUILandPlan

--- KMAUI/UI Items/Consumer UI/UITableViewCells/KMAUILotteryBasicInfoTableViewCell.swift
+++ KMAUI/UI Items/Consumer UI/UITableViewCells/KMAUILotteryBasicInfoTableViewCell.swift
@@ -14,18 +14,24 @@ public class KMAUILotteryBasicInfoTableViewCell: UITableViewCell {
     @IBOutlet public weak var bgView: KMAUIRoundedCornersView!
     @IBOutlet public weak var statusLabel: KMAUIBoldTextLabel!
     @IBOutlet public weak var titleLabel: KMAUIBoldTextLabel!
-    @IBOutlet public weak var subLandsLabel: KMAUIRegularTextLabel!
-    @IBOutlet public weak var subLandsCountLabel: KMAUIBoldTextLabel!
+    @IBOutlet public weak var stackViewBg: UIView!
+    @IBOutlet public weak var stackViewBgTop: NSLayoutConstraint!
+    @IBOutlet public weak var stackView: UIStackView!
+    @IBOutlet public weak var viewDetailsButton: UIButton!
+    @IBOutlet public weak var viewDetailsButtonTop: NSLayoutConstraint!
+    @IBOutlet public weak var viewDetailsButtonHeight: NSLayoutConstraint!
     
     // MARK: - Variables
-    public static let id = "KMAUILotteryBasicInfoTableViewCell"
+    public var isExpanded = false
+    public static let id = "KMAUILotteryBasicInfoTableViewCell1"
+    public var selectedSegment = 0
     public var lottery = KMAUILandPlanStruct() {
         didSet {
             setupCell()
         }
     }
     public var canHighlight = false
-
+    
     override public func awakeFromNib() {
         super.awakeFromNib()
         
@@ -45,16 +51,17 @@ public class KMAUILotteryBasicInfoTableViewCell: UITableViewCell {
         // Title label
         titleLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(24)
         
-        // Sub lands label
-        subLandsLabel.font = KMAUIConstants.shared.KMAUIRegularFont.withSize(16)
-        
-        // Sub lands count label
-        subLandsCountLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(16)
+        // viewDetails button
+        viewDetailsButton.backgroundColor = KMAUIConstants.shared.KMAUILightButtonColor
+        viewDetailsButton.titleLabel?.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(17)
+        viewDetailsButton.setTitleColor(KMAUIConstants.shared.KMAUITextColor, for: .normal)
+        viewDetailsButton.layer.cornerRadius = 17
+        viewDetailsButton.clipsToBounds = true
         
         // No standard selection requried
         selectionStyle = .none
     }
-
+    
     override public func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
@@ -76,6 +83,24 @@ public class KMAUILotteryBasicInfoTableViewCell: UITableViewCell {
     }
     
     public func setupCell() {
+        // Hide view details
+        hideViewDetails()
+        // Setup data rows
+        var rows = [KMAUIRowData]()
+        // Check the segment
+        if selectedSegment == 0 {
+            // Sub lands count, citizens count
+            rows = [KMAUIRowData(rowName: "Sub lands", rowValue: "\(lottery.subLandsCount)"), KMAUIRowData(rowName: "Citizens", rowValue: "\(lottery.queueCount)")]
+        } else if selectedSegment == 1 {
+            // Sub lands count, citizens count, View details button
+            rows = [KMAUIRowData(rowName: "Sub lands", rowValue: "\(lottery.subLandsCount)"), KMAUIRowData(rowName: "Citizens", rowValue: "\(lottery.queueCount)")]
+            if lottery.lotteryStatus == "Approved to start" {
+                showViewDetails()
+            }
+        } else if selectedSegment == 2 {
+            // Medal on top, land size percent, land id, address, decline or accept land / declined
+        }
+        
         // Lottery status
         statusLabel.text = lottery.lotteryStatus.addGaps()
         statusLabel.backgroundColor = KMAUIUtilities.shared.lotteryColor(status: lottery.lotteryStatus)
@@ -83,7 +108,80 @@ public class KMAUILotteryBasicInfoTableViewCell: UITableViewCell {
         // Lottery name
         titleLabel.text = lottery.landName
         
-        // Sub lands count
-        subLandsCountLabel.text = "\(lottery.subLandsCount)"
+        // Stack view
+        stackViewBg.backgroundColor = UIColor.clear
+        setupStackView(rows: rows)
+    }
+    
+    /**
+     Hide view details
+     */
+    
+    func hideViewDetails() {
+        viewDetailsButton.alpha = 0
+        viewDetailsButtonTop.constant = 0
+        viewDetailsButtonHeight.constant = 0
+    }
+    
+    /**
+     Show view details
+     */
+    
+    func showViewDetails() {
+        viewDetailsButton.alpha = 1
+        viewDetailsButtonTop.constant = 24
+        viewDetailsButtonHeight.constant = 34
+    }
+    
+    /**
+     Show the stackView
+     */
+    
+    public func setupStackView(rows: [KMAUIRowData]) {
+        // Remove subviews
+        for subview in stackView.subviews {
+            stackView.removeArrangedSubview(subview)
+            subview.removeFromSuperview()
+        }
+        
+        // Prepare the rows
+        for (index, row) in rows.enumerated() {
+            let itemView = UIStackView()
+            itemView.axis = .horizontal
+            itemView.distribution = UIStackView.Distribution.fill
+            itemView.alignment = UIStackView.Alignment.fill
+            itemView.spacing = 4
+            
+            // Row name label
+            let rowNameLabel = KMAUIRegularTextLabel()
+            rowNameLabel.font = KMAUIConstants.shared.KMAUIRegularFont.withSize(16)
+            rowNameLabel.textAlignment = .left
+            rowNameLabel.text = row.rowName
+            rowNameLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 251), for: .horizontal)
+            itemView.addArrangedSubview(rowNameLabel)
+            
+            // Row value label
+            let rowValueLabel = KMAUIBoldTextLabel()
+            rowValueLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(16)
+            rowValueLabel.textAlignment = .right
+            rowValueLabel.text = row.rowValue
+            rowValueLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 252), for: .horizontal)
+            itemView.addArrangedSubview(rowValueLabel)
+            
+            stackView.addArrangedSubview(itemView)
+            itemView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0).isActive = true
+            
+            if index < rows.count - 1 {
+                // Line view
+                let lineView = UIView()
+                lineView.backgroundColor = KMAUIConstants.shared.KMAUIGreyLineColor.withAlphaComponent(0.2)
+                lineView.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
+                stackView.addArrangedSubview(lineView)
+                lineView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0).isActive = true
+                lineView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0).isActive = true
+            }
+        }
+        stackViewBg.alpha = 1
+        stackViewBgTop.constant = 16
     }
 }
