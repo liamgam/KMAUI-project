d4019326a86369b3924af96f5753a440a486399d
liamgam <liam.liamgamble@gmail.com>
2020-05-22 20:35:47 +0300

    2 new cell for Land Cases added

---
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIJudgeTableViewCell.swift
@@ -0,0 +1,95 @@
+//
+//  KMAUIJudgeTableViewCell.swift
+//  KMA
+//
+//  Created by Stanislav Rastvorov on 22.05.2020.
+//  Copyright © 2020 Office Mac. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+public class KMAUIJudgeTableViewCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    @IBOutlet public weak var bgView: KMAUIRoundedCornersView!
+    @IBOutlet public weak var profileImageView: UIImageView!
+    @IBOutlet public weak var nameLabel: UILabel!
+    @IBOutlet public weak var titleLabel: UILabel!
+    @IBOutlet public weak var caseNumberLabel: UILabel!
+    @IBOutlet public weak var dateLabel: UILabel!
+    
+    // MARK: - Variables
+    public static let id = "KMAUIJudgeTableViewCell"
+    public var landCase = KMAUILandCaseStruct() {
+        didSet {
+            setupCell()
+        }
+    }
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Larger shadow for bgView
+        bgView.layer.shadowOffset = CGSize(width: 0, height: 7)
+        bgView.layer.shadowRadius = 8
+        
+        // Name label
+        nameLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(20)
+        
+        // Title label
+        titleLabel.font = KMAUIConstants.shared.KMAUIRegularFont.withSize(16)
+        
+        // Case number
+        caseNumberLabel.font = KMAUIConstants.shared.KMAUIRegularFont.withSize(16)
+        
+        // Date
+        dateLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(16)
+        
+        // Background view
+        backgroundColor = KMAUIConstants.shared.KMAUIViewBgColorReverse
+        
+        // No selection required
+        selectionStyle = .none
+    }
+    
+    public func setupCell() {
+        // Name label
+        nameLabel.text = landCase.judge.fullName
+        // Title label
+        titleLabel.text = "Judge"
+        // Case number label
+        caseNumberLabel.text = "case #\(landCase.caseNumber) –"
+        // Date label
+        dateLabel.text = KMAUIUtilities.shared.formatStringShort(date: landCase.date)
+        // Citizen image
+        profileImageView.image = KMAUIConstants.shared.profilePlaceholder.withRenderingMode(.alwaysTemplate)
+        profileImageView.tintColor = KMAUIConstants.shared.KMAUILightBorderColor
+        profileImageView.layer.cornerRadius = 22
+        profileImageView.layer.borderColor = KMAUIConstants.shared.KMAUIGreyLineColor.cgColor
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.borderWidth = 2
+        profileImageView.layer.borderColor = KMAUIConstants.shared.KMAUILightBorderColor.cgColor
+        profileImageView.kf.indicatorType = .activity
+        profileImageView.contentMode = .scaleAspectFill
+        
+        if !landCase.judge.profileImage.isEmpty, let url = URL(string: landCase.judge.profileImage) {
+            profileImageView.kf.setImage(with: url) { result in
+                switch result {
+                case .success(let value):
+                    self.profileImageView.image = value.image
+                    self.profileImageView.layer.borderWidth = 0
+                case .failure(let error):
+                    print(error.localizedDescription) // The error happens
+                }
+            }
+        }
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ KMAUI/UI Items/Ministry UI/UITableViewCells/KMAUIMinistryDecisionStatisticsTableViewCell.swift
@@ -0,0 +1,142 @@
+//
+//  KMAUIJudgeTableViewCell.swift
+//  KMA
+//
+//  Created by Stanislav Rastvorov on 22.05.2020.
+//  Copyright © 2020 Office Mac. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+public class KMAUIMinistryDecisionStatisticsTableViewCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    @IBOutlet public weak var bgView: KMAUIRoundedCornersView!
+    @IBOutlet public weak var logoImageView: UIImageView!
+    @IBOutlet public weak var titleLabel: UILabel!
+    @IBOutlet public weak var percentLabel: UILabel!
+    @IBOutlet public weak var approvedCountLabel: UILabel!
+    @IBOutlet public weak var rejectedCountLabel: UILabel!
+    @IBOutlet public weak var approvedLineView: UIView!
+    @IBOutlet public weak var rejectedLineView: UIView!
+    @IBOutlet public weak var statsViewWidth: NSLayoutConstraint!
+    @IBOutlet public weak var approvedWidth: NSLayoutConstraint!
+    @IBOutlet public weak var rejectedWidth: NSLayoutConstraint!
+    
+    // MARK: - Variables
+    public static let id = "KMAUIMinistryDecisionStatisticsTableViewCell"
+    public var decisions = [KMAUIMinistryDecisionStruct]() {
+        didSet {
+            setupCell()
+        }
+    }
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Larger shadow for bgView
+        bgView.layer.shadowOffset = CGSize(width: 0, height: 7)
+        bgView.layer.shadowRadius = 8
+        
+        // Name label
+        titleLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(20)
+        
+        // Case number
+        percentLabel.font = KMAUIConstants.shared.KMAUIRegularFont.withSize(16)
+
+        // Background view
+        backgroundColor = KMAUIConstants.shared.KMAUIViewBgColorReverse
+        
+        // Approved count label
+        approvedCountLabel.font = KMAUIConstants.shared.KMAUIBoldFont
+        
+        // Rejected count label
+        rejectedCountLabel.font = KMAUIConstants.shared.KMAUIBoldFont
+                
+        // No selection required
+        selectionStyle = .none
+    }
+    
+    public func setupCell() {
+        var width: CGFloat = 280
+        
+        if UIDevice.current.orientation.isLandscape {
+            print("Landscape")
+        } else {
+            print("Portrait")
+            width = 220
+        }
+        
+        // Name label
+        titleLabel.text = "Statistics"
+        // Title label
+        percentLabel.text = "Valid decisions – 100%"
+        // Citizen image
+        logoImageView.image = KMAUIConstants.shared.citizensIcon
+        // Approved vs Rejected
+        var approvedCount = 0
+        var rejectedCount = 0
+        
+        for decision in decisions {
+            if decision.ministryStatus == "approved" {
+                approvedCount += 1
+            } else {
+                rejectedCount += 1
+            }
+        }
+                
+        var approvedPercent: Double = 0
+        var rejectedPercent: Double = 0
+        
+        statsViewWidth.constant = width
+        approvedWidth.constant = width / 2 - 2
+        rejectedWidth.constant = width / 2 - 2
+        
+        if approvedCount > 0, rejectedCount > 0 {
+            approvedPercent = Double(Int((Double(approvedCount) / Double(approvedCount + rejectedCount)) * 100)) / 100
+            rejectedPercent = 1 - approvedPercent
+            approvedLineView.alpha = 1
+            rejectedLineView.alpha = 1
+            
+            if approvedPercent > 0.8 {
+                approvedWidth.constant = (width - 2) * 0.85
+                rejectedWidth.constant = width - 2 - approvedWidth.constant
+            } else if rejectedPercent > 0.8 {
+                rejectedWidth.constant = (width - 2) * 0.85
+                approvedWidth.constant = width - 2 - rejectedWidth.constant
+            } else {
+                approvedWidth.constant = (width - 2) * CGFloat(approvedPercent)
+                rejectedWidth.constant = width - 2 - approvedWidth.constant
+            }
+        } else if approvedPercent > 0 {
+            approvedPercent = 1
+            rejectedPercent = 0
+            approvedLineView.alpha = 1
+            rejectedLineView.alpha = 0
+            approvedWidth.constant = width
+            rejectedWidth.constant = 0
+        } else if rejectedCount > 0 {
+            approvedPercent = 0
+            rejectedPercent = 1
+            approvedLineView.alpha = 0
+            rejectedLineView.alpha = 1
+            approvedWidth.constant = 0
+            rejectedWidth.constant = width
+        }
+
+        approvedCountLabel.text = "\(Int(approvedPercent * 100))%"
+        rejectedCountLabel.text = "\(Int(rejectedPercent * 100))%"
+        
+        print("\(approvedWidth.constant) - \(rejectedWidth.constant)")
+    }
+    
+    
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
