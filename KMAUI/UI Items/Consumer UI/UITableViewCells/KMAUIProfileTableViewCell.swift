c3a25414d99c47b8e56889f6716fbcf0118fd065
liamgam <liam.liamgamble@gmail.com>
2020-06-03 13:30:16 +0300

    KMAUIProfileTableViewCell multiline support

--- KMAUI/UI Items/Consumer UI/UITableViewCells/KMAUIProfileTableViewCell.swift
+++ KMAUI/UI Items/Consumer UI/UITableViewCells/KMAUIProfileTableViewCell.swift
@@ -52,12 +52,12 @@ public class KMAUIProfileTableViewCell: UITableViewCell {
         // Setup usernameLabel
         usernameLabel.text = ""
         usernameLabel.textColor = KMAUIConstants.shared.KMABrightBlueColor
-        usernameLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(22)
+//        usernameLabel.font = KMAUIConstants.shared.KMAUIBoldFont.withSize(22)
         usernameLabel.minimumScaleFactor = 0.5
         
         // Setup fullNameLabel
         fullNameLabel.text = ""
-        fullNameLabel.font = KMAUIConstants.shared.KMAUIRegularFont.withSize(16)
+//        fullNameLabel.font = KMAUIConstants.shared.KMAUIRegularFont.withSize(16)
         fullNameLabel.minimumScaleFactor = 0.5
         
         // Setup the right arrow
