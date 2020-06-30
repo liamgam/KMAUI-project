f1ed4a5964f7177966b1d1bac6d7f4fa062379fa
liamgam <liam.liamgamble@gmail.com>
2020-06-30 15:21:44 +0300

    Separate arrays for notifications: land lottery, land cases, trespass cases

--- KMAUI/UI Items/KMAUIConstants.swift
+++ KMAUI/UI Items/KMAUIConstants.swift
@@ -277,7 +277,7 @@ public class KMAUIConstants {
     let placeholderApprovedDecisions = ["This Land Case matches all the rules required by the *departmentName*. It's verified to be resolved as \"Approved\".", "The *departmentName* confirms the Land Case has no violations and sets the status to be \"Approved\".", "After checking all of the documents provided by the Land Case appointment the *departmentName* can guarantee it doesn't violate any rules and can be recommended for the successfull Department and Court decision."]
     let placeholderRejectedDecisions = ["The Land Case can not be approved as we see it violates several rules from the list used by the *departmentName*. We recommend the documents to be revised before proceeding with the repeated appointment.", "The *departmentName* has checked the documents provided with the Land Case appointment and cannot guarantee these are the authentic data.", "The Land Case details doesn't include the full list of documents required by the *departmentName*. Please revise the provided documentation."]
     
-    // Regions dict
+    // MARK: - Regions dict
     let regionsDict = ["Jazan": "uNaTZ905Cg",
                        "Riyadh": "fN4QkVTtYG",
                        "Al Baha": "0MeC5xWrhK",
@@ -305,4 +305,9 @@ public class KMAUIConstants {
 //    11. Northern Borders: l6XhWzfDpe - Al Hudud Ash Shamaliyah
 //    12. Makkah: jKSqzVZ1Qh - Makkah
 //    13. Eastern Province: U3pCS8McNA - Ash Sharqiyah
+    
+    // MARK: - Notification arrays
+    var landLotteryNotifications = [KMANotificationStruct]()
+    var landCasesNotifications = [KMANotificationStruct]()
+    var trespassCasesNotifications = [KMANotificationStruct]()
 }
